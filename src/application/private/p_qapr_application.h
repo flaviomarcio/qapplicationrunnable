1b4acdd91c0bbd30b6860ddd1f833853bab669f7
Magma-Flavio Portela <fmspx@hotmail.com>
2021-12-28 06:35:53 -0300

    refatoracao

--- src/application/private/p_qapr_application.h
+++ src/application/private/p_qapr_application.h
@@ -22,6 +22,7 @@ public:
     QVariantHash __arguments;
     QRpc::ServiceManager manager;
     Application*application=nullptr;
+
     explicit ApplicationPvt(Application*parent=nullptr):QObject(parent),circuitBreaker(parent)
     {
         if(applicationSettingDir.isEmpty())
@@ -31,25 +32,24 @@ public:
 
     QVariant settings_SERVER()
     {
-        if(_settings_SERVER.isNull()){
-            QStringList vList;
-            {
-                auto settingsFile=qsl("%1.json").arg(qApp->applicationFilePath());
-                if(!QFile::exists(settingsFile))
-                    settingsFile=qsl("%1/%2").arg(applicationSettingDir,settings_SERVER_FILE);
+        if(!_settings_SERVER.isNull())
+            return this->_settings_SERVER;
+        QStringList vList;
+        {
+            auto settingsFile=qsl("%1.json").arg(qApp->applicationFilePath());
+            if(!QFile::exists(settingsFile))
+                settingsFile=qsl("%1/%2").arg(applicationSettingDir,settings_SERVER_FILE);
 
-                if(QFile::exists(settingsFile))
-                    vList<<settingsFile;
-            }
-
-            {
-                auto settingsFile=qsl("%1/%2/%3.json").arg(settings_HOME_DIR,qAppName(),settings_SERVER_FILE);
-                if(QFile::exists(settingsFile))
-                    vList<<settingsFile;
-            }
-            this->_settings_SERVER=vList;
+            if(QFile::exists(settingsFile))
+                vList<<settingsFile;
         }
-        return this->_settings_SERVER;
+
+        {
+            auto settingsFile=qsl("%1/%2/%3.json").arg(settings_HOME_DIR,qAppName(),settings_SERVER_FILE);
+            if(QFile::exists(settingsFile))
+                vList<<settingsFile;
+        }
+        return (this->_settings_SERVER=vList);
     }
 
     static void resourceExtract()
@@ -58,61 +58,64 @@ public:
         if(!dirHome.exists())
             dirHome.mkpath(applicationSettingDir);
 
-        if(dirHome.exists()){
-            QDir dir(qsl(":"));
-            dir.setNameFilters(QStringList{qsl("*.json")});
-            for(auto&info:dir.entryInfoList()){
-                QFile fileSrc(info.filePath());
-                QFile fileDst(qsl("%1/%2").arg(applicationSettingDir,info.fileName()));
-                if(fileDst.exists())
-                    continue;
+        if(dirHome.exists())
+            return;
 
-                if(!fileSrc.open(fileSrc.ReadOnly)){
+        QDir dir(qsl(":"));
+        dir.setNameFilters(QStringList{qsl("*.json")});
+        for(auto&info:dir.entryInfoList()){
+            QFile fileSrc(info.filePath());
+            QFile fileDst(qsl("%1/%2").arg(applicationSettingDir,info.fileName()));
+            if(fileDst.exists())
+                continue;
+
+            if(!fileSrc.open(fileSrc.ReadOnly)){
 #if Q_RPC_LOG
-                    sWarning()<<qsl("No open file:")<<fileSrc.fileName()<<qsl(", error: ")<<fileSrc.errorString();
+                sWarning()<<qsl("No open file:")<<fileSrc.fileName()<<qsl(", error: ")<<fileSrc.errorString();
 #endif
-
-                }
-                if(!fileDst.open(fileDst.Truncate | fileDst.Unbuffered | fileDst.WriteOnly)){
-#if Q_RPC_LOG
-                    sWarning()<<qsl("No open file:")<<fileDst.fileName()<<qsl(", error: ")<<fileDst.errorString();
-#endif
-
-                }
-
-                fileDst.write(fileSrc.readAll());
-                fileDst.close();
-                if(fileDst.isOpen())
-                    fileSrc.close();
+                continue;
             }
+            if(!fileDst.open(fileDst.Truncate | fileDst.Unbuffered | fileDst.WriteOnly)){
+#if Q_RPC_LOG
+                sWarning()<<qsl("No open file:")<<fileDst.fileName()<<qsl(", error: ")<<fileDst.errorString();
+#endif
+                fileSrc.close();
+                continue;
+            }
+
+            fileDst.write(fileSrc.readAll());
+            fileDst.flush();
+            fileDst.close();
+            fileSrc.close();
         }
     }
 
-    QVariantHash&arguments(){
-        if(__arguments.isEmpty()){
-            for(auto&v:qApp->arguments()){
-                auto l=v.split(qsl("="));
-                if(l.isEmpty()){
-                    continue;
-                }
+    QVariantHash&arguments()
+    {
+        if(!__arguments.isEmpty())
+            return __arguments;
 
-                if(l.size()==1){
-                    auto key=l.first();
-                    auto value=l.last();
-                    __arguments[key]=value;
-                }
-                else{
-                    auto key=l.first().toLower();
-                    auto value=l.last();
-                    __arguments[key]=value;
-                }
+        for(auto&v:qApp->arguments()){
+            auto l=v.split(qsl("="));
+            if(l.isEmpty())
+                continue;
+
+            if(l.size()==1){
+                auto key=l.first();
+                auto value=l.last();
+                __arguments[key]=value;
+                continue;
             }
 
-            QHashIterator<QString, QVariant> i(manager.arguments());
-            while (i.hasNext()) {
-                i.next();
-                __arguments[i.key()]=i.value();
-            }
+            auto key=l.first().toLower();
+            auto value=l.last();
+            __arguments[key]=value;
+        }
+
+        QHashIterator<QString, QVariant> i(manager.arguments());
+        while (i.hasNext()) {
+            i.next();
+            __arguments[i.key()]=i.value();
         }
         return __arguments;
     }
