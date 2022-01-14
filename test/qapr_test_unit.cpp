98f78f84656112b09a64be2357f6edea923ada12
Magma-Flavio Portela <fmspx@hotmail.com>
2022-01-14 17:49:14 -0300

    unit teste

--- test/qapr_test_unit.cpp
+++ test/qapr_test_unit.cpp
@@ -1,11 +1,11 @@
-#ifndef Q_APR_VariantUtilTestUnit_H
-#define Q_APR_VariantUtilTestUnit_H
+#ifndef Q_APR_TestUnit_H
+#define Q_APR_TestUnit_H
 
 #include "./qapr_test_unit.h"
 
 namespace QApr{
 
-class Q_APR_VariantUtilTestUnit : public SDKGoogleTestUnit {
+class Q_APR_TestUnit : public SDKGoogleTestUnit {
 public:
 };
 
