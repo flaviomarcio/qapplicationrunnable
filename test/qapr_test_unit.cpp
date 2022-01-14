5ab494507406ad550d4d0be76e738bf5c509e7e0
Magma-Flavio Portela <fmspx@hotmail.com>
2022-01-14 17:48:48 -0300

    testes

---
+++ test/qapr_test_unit.cpp
@@ -0,0 +1,14 @@
+#ifndef Q_APR_VariantUtilTestUnit_H
+#define Q_APR_VariantUtilTestUnit_H
+
+#include "./qapr_test_unit.h"
+
+namespace QApr{
+
+class Q_APR_VariantUtilTestUnit : public SDKGoogleTestUnit {
+public:
+};
+
+}
+
+#endif
