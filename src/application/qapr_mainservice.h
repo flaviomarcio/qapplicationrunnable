b0a424016cf1fda15bbdfdfd1d6e17a546a03b6f
Flavio Portela <fmspx@hotmail.com>
2022-09-20 14:28:00 -0300

    .

--- src/application/qapr_mainservice.h
+++ src/application/qapr_mainservice.h
@@ -2,12 +2,33 @@
 
 #include <QCoreApplication>
 
-#define QAPR_MAIN_DECLARE()\
+#ifdef QAPR_TEST
+#include <gmock/gmock.h>
+#include <QCoreApplication>
+#endif
+
+#define QAPR_MAIN_NORMAL_DECLARE() \
 int main(int argc, char* argv[])\
 {\
     QCoreApplication a(argc, argv);\
     return QApr::MainService(nullptr).exec(a);\
 }
+
+
+#define QAPR_MAIN_TEST_DECLARE() \
+int main(int argc, char* argv[])\
+{\
+    QCoreApplication a(argc, argv);\
+    testing::InitGoogleTest(&argc, argv);\
+    testing::InitGoogleMock(&argc, argv);\
+    return RUN_ALL_TESTS();\
+}
+
+#ifdef QAPR_TEST
+#define QAPR_MAIN_DECLARE() QAPR_MAIN_NORMAL_DECLARE()
+#else
+#define QAPR_MAIN_DECLARE() QAPR_MAIN_TEST_DECLARE()
+#endif
 
 namespace QApr{
 
