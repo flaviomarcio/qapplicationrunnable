c2e93efc59184db9e5615e57b808833ef0adef64
Flavio-Portela <fmspx@hotmail.com>
2023-01-22 18:55:53 -0300

    logs

--- src/services/qapr_agent_base.cpp
+++ src/services/qapr_agent_base.cpp
@@ -47,7 +47,7 @@ public slots:
 #endif
         if(!pool.get(db)){
 #if Q_APR_LOG
-            oInfo()<<QStringLiteral("%1, no connection db: %2").arg(pp.agentName(), pool.lastError().text());
+            sInfo()<<QStringLiteral("%1, no connection db: %2").arg(pp.agentName(), pool.lastError().text());
 #endif
             return;
         }
